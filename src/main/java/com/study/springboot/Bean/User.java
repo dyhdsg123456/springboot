96edb7a9e5a1aedaf2dba6ae7d48c2282b877896
duyuanhao <13775516233@163.com>
2019-06-21 14:03:14 +0800

    1

---
+++ src/main/java/com/study/springboot/Bean/User.java
@@ -0,0 +1,16 @@
+package com.study.springboot.Bean;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * Author: dyh
+ * Date:   2019/6/21
+ * Description:
+ */
+@Data
+@ToString
+public class User {
+    private String name;
+    private Integer age;
+}
--- src/test/java/com/study/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/study/springboot/SpringbootApplicationTests.java
@@ -1,16 +1,27 @@
 package com.study.springboot;
 
+import com.study.springboot.Bean.User;
+import com.study.springboot.util.RedisUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootApplicationTests {
+	@Autowired
+	RedisUtil redisUtil;
 
 	@Test
 	public void contextLoads() {
+		User user = new User();
+		user.setName("哈哈哈");
+		user.setAge(10);
+		redisUtil.set("user",user);
+		User user1 = (User) redisUtil.get("user");
+		System.out.println(user1);
 	}
 
 }
